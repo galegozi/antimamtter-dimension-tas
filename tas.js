fbc36f191a554fa2d0d2fd5e3240b2b179390d6b
Gal Egozi <geegozi@gmail.com>
2023-07-04 11:42:27 -0700

    Adding the intial files to setup the TAS.

---
+++ tas.js
@@ -0,0 +1,18 @@
+// click the button for the first antimatter dimension.
+// the button class is o-primary-btn o-primary-btn--new
+function clickButtonByClass(classname) {
+    document.querySelector(classname).click();
+}
+
+function clickFirstAntimamtterDimension() {
+    var classname = ".o-primary-btn.o-primary-btn--new";
+    clickButtonByClass(classname);
+}
+
+function toggleUntil10() {
+    var className = ".o-primary-btn.l-button-container"
+    var buttons = document.querySelectorAll(className);
+}
+
+// click the button for the first antimatter dimension.
+clickFirstAntimamtterDimension();
